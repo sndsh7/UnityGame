cd749d94c62b62e499e0e9aa01a2ad4b1bf2cd33
Sandesh Agawane <sndshagawane412@gmail.com>
2018-07-21 16:04:25 +0530

    image follow cursor position

---
+++ Assets/Scripts/ObjectFollowPointer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFollowPointer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+	}
+}
